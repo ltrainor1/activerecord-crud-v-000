9eeb6e39d1ae953ae75743bdcef99f1b829fae5c
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-21 23:58:15 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,14 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(args = {})
-  Movie.create({title: nil, release_date: nil, director: nil, lead: nil, in_theaters: nil})
+  Movie.create do |m|
+    m.title = args[:title],
+    m.release_date = args[:release_date],
+    m.director = args[:director],
+    m.lead = args[:lead],
+    m.in_theaters = args[:in_theaters]
+  end 
+    
   end
 
 
